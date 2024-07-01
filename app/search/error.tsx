287dc93bafeb3bdd22cefaf2af8b9e40b495d531
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-07-01 11:23:17 +0530

    handle errors for search term  and api requests failures

---
+++ app/search/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from 'next/link';
+import React from 'react'
+
+const Error = ({Error}:{Error:React.ReactNode}) => {
+  return (
+    <div className='flex flex-col justify-center items-center  text-2xl pt-10'>
+      Something Went Wrong or check your internet connection once !
+      <Link className='text-blue-500 text-xl' href="/">Try Again </Link>
+    </div>
+  )
+}
+
+export default Error
--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,8 +1,27 @@
+import Link from 'next/link';
 import React from 'react'
-
-const page = () => {
+const page =async ({searchParams}:{searchParams:{searchTerm:string}}) => {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  if(!response.ok){
+    throw new Error("something went wrong ! please try again")
+  }
+  const data =await response.json();
+  const results:{title:string}[] = data.items;
+  if(!results){
+    return(
+      <div className='flex flex-col justify-center items-center mt-9'>
+        <h1 className='text-3xl mb-4'>Nothing is found for the word {searchParams.searchTerm}!</h1>
+        <p className='flex flex-col items-center'>Try Again! By Navigating To Home Page
+          <Link href="/" className='text-blue-600'>Home</Link>
+        </p>
+      </div>
+    )
+  }
   return (
-    <div className='text-red-500'>webSearchpage</div>
+    <div>
+      {results&&results.map((result)=>(<h1 key={result?.title}>{result?.title}</h1>))}
+    </div>
+    
   )
 }
 
