00a3976f7ee4afda68934d2888c27a9d33afb04e
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-07-05 10:30:52 +0530

    nothing

--- app/search/image/page.tsx
+++ app/search/image/page.tsx
@@ -1,33 +1,46 @@
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
-const ImageSearch = dynamic(()=>import("@/app/components/ImageSearch"))
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { Suspense } from "react";
+const ImageSearch = dynamic(() => import("@/app/components/ImageSearch"));
 
-const page =async ({searchParams}:{searchParams:{searchTerm:string,start:string}}) => {
-  const start = searchParams.start || "1"
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${start}`);
-  if(!response.ok){
-    throw new Error("something went wrong ! please try again")
+const page = async ({
+  searchParams,
+}: {
+  searchParams: { searchTerm: string; start: string };
+}) => {
+  const start = searchParams.start || "1";
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${start}`
+  );
+  if (!response.ok) {
+    throw new Error("something went wrong ! please try again");
   }
-  const data =await response.json();
-  const results:{title:string}[] = data.items;
-  if(!results){
-    return(
-      <div className='flex flex-col justify-center items-center mt-9'>
-        <h1 className='text-3xl mb-4'>Nothing is found for the word {searchParams.searchTerm}!</h1>
-        <p className='flex flex-col items-center'>Try Again! By Navigating To Home Page
-          <Link href="/" className='text-blue-600'>Home</Link>
+  const data = await response.json();
+  const results: { title: string }[] = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center mt-9">
+        <h1 className="text-3xl mb-4">
+          Nothing is found for the word {searchParams.searchTerm}!
+        </h1>
+        <p className="flex flex-col items-center">
+          Try Again! By Navigating To Home Page
+          <Link href="/" className="text-blue-600">
+            Home
+          </Link>
         </p>
       </div>
-    )
+    );
   }
   return (
     <div>
-      {results&&
-      <ImageSearch results={data}/>
-      }
+      {results && (
+        <Suspense>
+          <ImageSearch results={data} />
+        </Suspense>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,33 +1,45 @@
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
-import React from 'react'
-const WebSearch = dynamic(()=>import("@/app/components/WebSearch"))
-const page =async ({searchParams}:{searchParams:{searchTerm:string,start:string}}) => {
-  const start = searchParams.start || "1"
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${start}`);
-  if(!response.ok){
-    throw new Error("something went wrong ! please try again")
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import React, { Suspense } from "react";
+const WebSearch = dynamic(() => import("@/app/components/WebSearch"));
+const page = async ({
+  searchParams,
+}: {
+  searchParams: { searchTerm: string; start: string };
+}) => {
+  const start = searchParams.start || "1";
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${start}`
+  );
+  if (!response.ok) {
+    throw new Error("something went wrong ! please try again");
   }
-  const data =await response.json();
-  const results:{title:string}[] = data.items;
-  if(!results){
-    return(
-      <div className='flex flex-col justify-center items-center mt-9'>
-        <h1 className='text-3xl mb-4'>Nothing is found for the word {searchParams.searchTerm}!</h1>
-        <p className='flex flex-col items-center'>Try Again! By Navigating To Home Page
-          <Link href="/" className='text-blue-600'>Home</Link>
+  const data = await response.json();
+  const results: { title: string }[] = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center mt-9">
+        <h1 className="text-3xl mb-4">
+          Nothing is found for the word {searchParams.searchTerm}!
+        </h1>
+        <p className="flex flex-col items-center">
+          Try Again! By Navigating To Home Page
+          <Link href="/" className="text-blue-600">
+            Home
+          </Link>
         </p>
       </div>
-    )
+    );
   }
   return (
     <div>
-      {results&&
-      <WebSearch results={data}/>
-      }
+      {results && (
+        <Suspense>
+          <WebSearch results={data} />
+        </Suspense>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default page
+export default page;
