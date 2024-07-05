44135b211d1ee675ce716ad963507bead1c8f100
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-07-05 10:23:58 +0530

    last commit

--- app/search/image/page.tsx
+++ app/search/image/page.tsx
@@ -1,5 +1,7 @@
-import ImageSearch from '@/app/components/ImageSearch';
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
+const ImageSearch = dynamic(()=>import("@/app/components/ImageSearch"))
+
 const page =async ({searchParams}:{searchParams:{searchTerm:string,start:string}}) => {
   const start = searchParams.start || "1"
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${start}`);
--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -1,6 +1,7 @@
-import WebSearch from '@/app/components/WebSearch';
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import React from 'react'
+const WebSearch = dynamic(()=>import("@/app/components/WebSearch"))
 const page =async ({searchParams}:{searchParams:{searchTerm:string,start:string}}) => {
   const start = searchParams.start || "1"
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.G_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${start}`);
